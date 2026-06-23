977ac2147e2205a835e93d760e5178106923e7de
Yashbuildz <junioryash321@gmail.com>
2026-06-23 10:48:07 +0530

    fix: studio page.tsx

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,15 +1,34 @@
+'use client'
+
 /**
  * app/studio/[[...tool]]/page.tsx
  */
 
+import dynamic from 'next/dynamic'
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../sanity.config'
 
-export const dynamic = 'force-static'
-export const revalidate = false
-
-export { metadata, viewport } from 'next-sanity/studio'
+const Studio = dynamic(
+  () => Promise.resolve(() => <NextStudio config={config} />),
+  {
+    ssr: false,
+    loading: () => (
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+        background: '#101010',
+        color: '#fff',
+        fontFamily: 'sans-serif',
+        fontSize: 14,
+      }}>
+        Loading Studio…
+      </div>
+    ),
+  }
+)
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <Studio />
 }
